3cb1974fae82b99f3e708c6f1664d2af8fbc1626
Nicole Gololobov <48893008+NicoleGololobov@users.noreply.github.com>
2020-01-21 15:18:48 -0500

    Add files via upload
    Add file with code
    Add file referenced in code
    Add files produced

---
+++ Excel_filter.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# reads file into a pandas data frame
+data = pd.read_excel("/Users/Nicole/Documents/orders.xls")
+
+# list unique values in column
+values = data.Item.unique()
+
+#
+# store data frames for each unique value in a dictionary
+values_dict = {i: pd.DataFrame for i in values}
+for k in values_dict.keys():
+    values_dict[k] = data[:][data.Item == k]
+
+# turn each dictionary key's data frame into its own file
+def make_xl(dict):
+    for k in dict.keys():
+        d = pd.DataFrame(dict[k])
+        d.to_excel(excel_writer="/Users/Nicole/Documents/item_" + k + ".xlsx")
+
+# call function
+make_xl(values_dict)
